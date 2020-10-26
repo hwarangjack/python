1b96001815714a8f69060aac6dc399653649ebdf
jang hyun joon <hwarang-hr@naver.com>
2020-10-26 15:52:31 +0900

    20201026

--- 1_Study/4. os 파일탐색/20201005_정규식에 맞는 파일 복사하기.py
+++ 1_Study/4. os 파일탐색/20201005_정규식에 맞는 파일 복사하기.py
@@ -3,8 +3,10 @@ import pandas as pd
 import shutil
 import re
 
-path=input(r'파일을 가져올 폴더의 절대경로를 지정하세요.(Python의 경우 \\을 기재하는 것에 유의) : ')
-how=input('정규식을 사용하여 조건을 설정하세요 :')
+
+# 대상파일 획정하기
+path='D:\\NaverCloud\\화랑\\☆ 자문'
+how='2020년\s*임금대장\s*.+[.]x+'
 
 empty_list=[]
 
@@ -14,18 +16,15 @@ def checkfile(path, target):
 
     for i in os.listdir(path):
         abs_path = os.path.join(path,i)
-        if os.path.isfile(abs_path):
+        if os.path.isfile(abs_path):    #os.path.join 함수는 절대경로가 있는 파일만 Read 할 수 있는 것 같음
             if cp.match(i):
-                empty_list.append([path,abs_path,i])
+                empty_list.append([abs_path,os.path.split(abs_path)[0],os.path.split(abs_path)[1]])
         
         elif os.path.isdir(abs_path):
             checkfile(abs_path, target)
-
-
 checkfile(path,how)
-# checkfile('D:\\NaverCloud\\화랑\\☆ 자문\\(99일) 홍정양행 ■ 폐업','2020년\s\d+.*임금대장.*[^세무].[.]x+')
-# checkfile('D:\\NaverCloud\\화랑\\☆ 자문','2020년\s*임금대장\s*.+[.]x+')
 
+# 대상파일을 현재작업폴더에 복사하기
 for i in empty_list:
-    shutil.copy(i[1],os.path.join(os.getcwd(),i[2]))
+    shutil.copy(i[0],os.path.join(os.getcwd(),i[2]))
 
