ef1402f7f5cbab0e5726c17344c07aa8773a1dac
jang hyun joon <hwarang-hr@naver.com>
2020-10-28 15:56:39 +0900

    20201028

--- 1_Study/5. django/practice_django/poll/models.py
+++ 1_Study/5. django/practice_django/poll/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class question(models.Model):
+    question_text = models.CharField(max_length=20)
+    pub_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class choice(models.Model):
+    question = models.ForeignKey(question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=20)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
