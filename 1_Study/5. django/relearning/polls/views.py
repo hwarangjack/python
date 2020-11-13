de14e04346ae06b2bbaa73b2284d1e1b09d9eced
jack <hwarang-hr@naver.com>
2020-11-13 14:25:22 +0900

    fisrst_step_complete

--- 1_Study/5. django/relearning/polls/views.py
+++ 1_Study/5. django/relearning/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question,Choice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     # Polls.models.py에서 정의된 Quesition 객체(Question.objects.all())를 date라는 컬럼 5개를 정렬해서 구성하여 변수에 할당
@@ -16,12 +18,35 @@ def index(request):
 
 def detail(request, question_id):   #detail에 연결되기 전 url에서는 <int:question_id>로 설정하여, url 매칭시 question_id라는 인자값을 전달토록 되어 있음
     # get_object_or_404 호출 : django.shortcuts에서 render와 함께 호출 필요.
-    # get_object_or_404 사용 : 첫번째 인자는 Model.py의 클래스(또는 함수) 명칭(Question)[=데이터베이스의 테이블명]이고, 두번쨰 인자값은 검색조건을 의미함.
+    # get_object_or_404 해석 : 특정한 객체를 가져와라(Try:) 오류가난다면 404에러를 발생시켜라(except:)
+        # try : instance_detail_question = Question.obejcts.get(pk=question_id)
+        # except : Question.DoesNotExist: Raise http404('question does not exist')
+    # get_object_or_404 사용 : 첫번째 인자는 사용하고자 하는 Model.py의 클래스(또는 함수) 명칭(Question)[=데이터베이스의 테이블명]이고, 두번째 인자값은 검색조건을 의미함.
     # get_object_or_404 역활 : 모델(테이블명)과 그 조건에 따라 검색한 결과(객체, Row)를 할당하라
     instance_detail_question = get_object_or_404(Question, pk=question_id)
 
     # 실제 값이 할당된 객체 instance_detail_question를 // template에서 사용할 teamplate_detail_question에 매핑(매칭)하고 // detail.html로 이동하라
     return render(request, 'polls/detail.html', {'template_detail_question':instance_detail_question})
 
-def vote(request):
-    return hello
+def vote(request, question_id):
+    instance_detail_question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        # instance_detail_question이 대상 테이블(이 경우 choice)이 연결(Foreignkey)되어 있는 테이블(choice)에 설정된 DB값을(_set)을 검색하여 가져와라(.get). 검색조건은 Post 제출된 choice의 id에 해당하는 값(Row)
+        selected_choice = instance_detail_question.choice_set.get(pk=request.POST['choice'])
+
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'poll/detail.html', {'template_detail_question':instance_detail_question})
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # reverse() 함수 : 첫번쨰 인자값 URL 패턴(polls/results) / 두번째 인자값 해당 URL 패턴에서 사용되는 인자값(instance_detail_question.id)
+        # reverse() 활용 : 함수에 활용된 URL 스트링으로 사용하여 url로 이동하라
+        return HttpResponseRedirect(reverse('polls:results', args=(instance_detail_question.id,)))
+
+
+def results(request, question_id):
+    instance_detail_question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'template_detail_question':instance_detail_question})
