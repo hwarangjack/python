eb89542b6947763dff4b4dac7adf3b01325911f7
jack <hwarang-hr@naver.com>
2020-11-14 18:25:14 +0900

    20201114

--- 1_Study/5. django/relearning/polls/admin.py
+++ 1_Study/5. django/relearning/polls/admin.py
@@ -4,12 +4,35 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
+# class choice_in_line(admin.StackedInline):
+#     model=Choice
+#     extra=2
+
+class choice_in_line(admin.TabularInline):
+    model=Choice
+    extra=2
+
 class questionadmin(admin.ModelAdmin):
     fieldsets = [
         ('question state', {'fields':['question_text']}),
         ('date information', {'fields':['date'], 'classes':['collapse']}),
     ]
+
+    # admin 사이트에서 칼럼명을 보이도록 하기
+    list_display = ('question_text','date')
+
+    # admin 사이트 우측에 옵션제공
+    list_filter = ['date']
+
+    # admin 사이트에 검색어 입력하기 (검색기준 칼럼)
+    search_fields = ['question_text']
+
+    # admin 사이트에서 choice 모델도 함께보기
+    inlines=[choice_in_line]
+
     
+
+
 # Admin.site.resiger(테이블명)을 사용하여 어드민 홈페이지에 DB에 등록된 테이블을 등록시킨다. 
 admin.site.register(Question, questionadmin)
 admin.site.register(Choice)
