686d0aa9307ee9fe75e79bbd8c82d394fe689a14
jack <hwarang-hr@naver.com>
2020-11-19 21:14:26 +0900

    20201119

--- 1_Study/5. django/relearning/board/models.py
+++ 1_Study/5. django/relearning/board/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class register(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateTimeField()
-    etc = models.TextField(blank=True, null=True)
+    etc = models.TextField(blank=True, null=False)
