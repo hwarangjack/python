1b8efca7b59834657cff0937e374070516299d57
home <hwarang-hr@naver.com>
2021-01-21 14:11:51 +0900

    20210121

--- 2_Project/1. 자동다운로드(pyg, selenium)/2. 국민연금/20201006_Auto_download_NPS.py
+++ 2_Project/1. 자동다운로드(pyg, selenium)/2. 국민연금/20201006_Auto_download_NPS.py
@@ -12,7 +12,7 @@ pyg.confirm('프로그램을 실행합니다.')
 
 #공통데이터에서 자료 추출하기
 abs_path=r'D:\\NaverCloud\\화랑\\매출(자문).xlsm'
-read_df1=pd.read_excel(abs_path, sheet_name='2020 New', skiprows=6)
+read_df1=pd.read_excel(abs_path, sheet_name='2021', skiprows=6)
 df=pd.DataFrame(read_df1)
 read_df2=df.iloc[:,[0,1,3,4,12]]                                      # 1차 가공) 일부 데이터만 사용하기
 df2=pd.DataFrame(read_df2)                                                  # 가공데이터 DF로 배정하기 (자동완선기능 사용)
