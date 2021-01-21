e7dfe05444d6b0dbc32720b0889c1b9af8d82386
home <hwarang-hr@naver.com>
2021-01-21 17:10:07 +0900

    20210121

--- 2_Project/2. 파일이름변경(os, pandas)/20210121_FileName_change.py
+++ 2_Project/2. 파일이름변경(os, pandas)/20210121_FileName_change.py
@@ -19,7 +19,7 @@ final_df.set_index('사업장관리번호',inplace=True)
 
 # Middle Name 수정
 for i in os.listdir('.'):
-    if i.startswith(yyyymm) and i.endswith('.xls'):
+    if i.startswith(yyyymm) and (i.endswith('.xls') or i.endswith('.xlsx')):
         try:
             j=i.split(' ')
             k=i.replace(j[1],final_df.loc[int(j[1])].loc['사업장명'])
@@ -27,31 +27,31 @@ for i in os.listdir('.'):
                 print(k+'의 파일이 이미 존재합니다')
             else:
                 os.rename(i,k)
+                print(k)
         except:
             print(i+'파일은 변경내용이 없거나, 사업장관리번호가 존재하지 않습니다.')
 
-print('------------------Middle name 변경 완료------------------')
-print('------------------Middle name 변경 완료------------------')
-
 # # Last Name 수정
 for i in os.listdir('.'):
-    if i.startswith(yyyymm) and i.endswith('rjsrkd.xls'):
+    if i.startswith(yyyymm) and (i.endswith('rjsrkd.xls') or i.endswith('rjsrkd.xlsx')):
         try:
             j=i.replace('rjsrkd','건강')
             if j in os.listdir('.'):
                 print(j+'의 파일이 이미 존재합니다.')
             else:
                 os.rename(i,j)
+                print(j)
         except:
             print(i+'파일은 변경내용이 없습니다')
 
-    if i.startswith(yyyymm) and i.endswith('dusrma.xls'):
+    if i.startswith(yyyymm) and (i.endswith('dusrma.xls') or i.endswith('dusrma.xlsx')):
         try:
-            j=i.replace('rjsrkd','연금')
+            j=i.replace('dusrma','연금')
             if j in os.listdir('.'):
                 print(j+'의 파일이 이미 존재합니다.')
             else:
                 os.rename(i,j)
+                print(j)
         except:
             print(i+'파일은 변경내용이 없습니다')
     
