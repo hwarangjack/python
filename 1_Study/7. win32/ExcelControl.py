78c49b85e767a8d0f34b5580bb18011550182bbf
jang hyun joon <hwarang-hr@naver.com>
2021-01-26 12:03:50 +0900

    clear

---
+++ 1_Study/7. win32/ExcelControl.py
@@ -0,0 +1,46 @@
+import re
+import os
+import win32com.client
+import time
+
+
+# 수정파일 확정하기
+empty_list=[]
+
+def checkfile(path, target):
+    cp=re.compile(target)
+    for i in os.listdir(path):
+        abs_path = os.path.join(path,i)
+        if os.path.isfile(abs_path):    #os.path.join 함수는 절대경로가 있는 파일만 Read 할 수 있는 것 같음
+            if cp.match(i):
+                empty_list.append(abs_path)
+        elif os.path.isdir(abs_path):
+            checkfile(abs_path, target)
+
+path='D:\\NaverCloud\\화랑\\☆ 자문'
+how='2021년\s*임금대장\s*.+[.]x+'
+checkfile(path,how)
+
+
+# 파일접근
+
+excel = win32com.client.Dispatch('Excel.Application')
+excel.Visible = True
+excel.displayalerts = False
+
+for i in empty_list:
+    wb = excel.workbooks.Open(i)
+    ws = wb.worksheets('(조건)')
+    ws.cells(6,16).value = 0.1152
+    ws.cells(16,18).value = 0.1152
+    ws.cells(6,16).interior.colorindex = 27
+    ws.cells(16,18).interior.colorindex = 27
+    abs_name = os.path.split(i)
+    dirpath = abs_name[0]
+    filename = os.path.splitext(abs_name[1])[0]+"(py)"
+    filedot = os.path.splitext(abs_name[1])[1]
+    full_name = os.path.join(dirpath,filename+filedot)
+    wb.SaveAs(os.path.join(dirpath,filename+filedot))
+    print(os.path.basename(i)+' 완료')
+
+    wb.Close()
